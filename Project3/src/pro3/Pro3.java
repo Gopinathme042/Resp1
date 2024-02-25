0fc41ce4559757e74b3d2e13a99ed5648af35ee9
gopinath.elumalai <gopinath.elumalai@SPBLRLAP-611.speridian.com>
2024-02-25 13:27:59 +0530

    Commit 2

---
+++ Project3/src/pro3/Pro3.java
@@ -0,0 +1,15 @@
+package pro3;
+
+public class Pro3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bye");
+		add();
+	}
+
+public static	void add()
+	{
+		System.out.println(2+3);
+	}
+}
